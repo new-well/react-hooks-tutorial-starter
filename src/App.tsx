b89d269ef275dc61bfaebe19e1e9d12cb2b28eac
new-well <takahiro.arai.sb@gmail.com>
2021-08-22 16:01:05 +0900

    [update] 本の追加機能

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import BookRow from "./BookRow";
 import BookSearchDialog from "./BookSearchDialog";
 // types
 import { BookToRead } from "./BookToRead";
+import { BookDescription } from "./BookDescription";
+
 
 Modal.setAppElement("#root");
 
@@ -23,29 +25,8 @@ const customStyles = {
   }
 };
 
-const dummyBooks: BookToRead[] = [
-  {
-    id: 1,
-    title: "はじめてのReact",
-    authors: "ダミー",
-    memo: ""
-  },
-  {
-    id: 2,
-    title: "React Hooks入門",
-    authors: "ダミー",
-    memo: ""
-  },
-  {
-    id: 3,
-    title: "実践Reactアプリケーション開発",
-    authors: "ダミー",
-    memo: ""
-  }
-];
-
 const App = () => {
-  const [books, setBooks] = useState(dummyBooks);
+  const [books, setBooks] = useState([] as BookToRead[]);
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
   const handleBookMemoChange = (id: number, memo: string) => {
@@ -55,6 +36,13 @@ const App = () => {
         : b;
     });
     setBooks(newBooks);
+  };
+
+  const handleBookAdd = (book: BookDescription) => {
+    const newBook: BookToRead = {...book, id: Date.now(), memo: "" };
+    const newBooks = [...books, newBook];
+    setBooks(newBooks);
+    setModalIsOpen(false);
   };
 
   const handleBookDelete = (id: number) => {
@@ -93,7 +81,7 @@ const App = () => {
         onRequestClose={handleModalClose}
         style={customStyles}
       >
-        <BookSearchDialog maxResults={20} onBookAdd={(b) => {}} />
+        <BookSearchDialog maxResults={20} onBookAdd={(b) => handleBookAdd(b)} />
       </Modal>
     </div>
   );
